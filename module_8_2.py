dd61527a04e9eb03321ed2b4733c831df8be988d
user <sasha.sapegin.99@gmail.com>
2024-11-04 18:01:53 +0700

    02/11/2024

--- module_8_2.py
+++ module_8_2.py
@@ -1,3 +1,5 @@
+
+
 def personal_sum(numbers):
     global result, incorrect_data
     result = 0
@@ -8,23 +10,26 @@ def personal_sum(numbers):
                 result += numbers[i]
             except TypeError:
                 incorrect_data += 1
-        print((result, incorrect_data))
+                print(f'Некорректный тип данных для подсчёта суммы - {numbers[i]}')
+        return result, incorrect_data
     except TypeError:
-        print(0)
+        return 0
 
 
 def calculate_average(numbers):
     personal_sum(numbers)
     try:
-        print(result / (len(numbers) - incorrect_data))
+        return result / (len(numbers) - incorrect_data)
     except ZeroDivisionError:
-        print(0)
+        return 0
     except  TypeError:
-        print('В numbers записан некорректный тип данных')
+        return 'В numbers записан некорректный тип данных'
 
 
-calculate_average("1, 2, 3")
-calculate_average([1, "Строка", 3, "Ещё Строка"])
-calculate_average(567)
-calculate_average([42, 15, 36, 13])
-calculate_average([])
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
+print(f'Результат 5: {calculate_average([])}')
