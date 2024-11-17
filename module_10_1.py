9a1e6c6416f1d037a77d3790e85d527e5651aa17
user <sasha.sapegin.99@gmail.com>
2024-11-17 21:07:35 +0700

    02/11/2024

---
+++ module_10_1.py
@@ -0,0 +1,40 @@
+import time
+import threading
+
+
+def write_words(word_count, file_name):
+    # with open()
+    file = open(file_name, 'a', encoding='utf-8')
+    for i in range(1, word_count + 1):
+        file.write(f'Какое-то слово № {i}\n')
+        time.sleep(0.1)
+    print(f'Завершилась запись в файл {file_name}')
+    file.close()
+
+
+start_time = time.time()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f'Работа в потоке {elapsed_time}  секунд')
+
+start_time = time.time()
+thread_1 = threading.Thread(target=write_words, args=(10, 'example5.txt'))
+thread_2 = threading.Thread(target=write_words, args=(30, 'example6.txt'))
+thread_3 = threading.Thread(target=write_words, args=(200, 'example7.txt'))
+thread_4 = threading.Thread(target=write_words, args=(100, 'example8.txt'))
+
+writers = [thread_1, thread_2, thread_3, thread_4]
+for writer in writers:
+    writer.start()
+
+for writer in writers:
+    writer.join()
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f'Работа в потоке {elapsed_time} секунд')
+
